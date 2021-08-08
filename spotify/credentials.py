f39e1182a460cfc517afd383ca9b6c02f1f2380e
EdwardX29 <69602889+EdwardX29@users.noreply.github.com>
2021-08-07 20:57:55 -0400

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "c39bb672bbfb4ec1a05b2f7901a720b6"
 CLIENT_SECRET = "dfb89889cceb48d7878bdbe5a13f1c6e"
-REDIRECT_URI = "https://biogtest.herokuapp.com/callback"
+REDIRECT_URI = "https://biographify.herokuapp.com/callback"
