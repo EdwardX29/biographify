bfde1cc7fe9ff9fb7079d160715d614e6577bf5e
EdwardX29 <69602889+EdwardX29@users.noreply.github.com>
2021-08-07 20:46:55 -0400

    Update urls.py

--- bio_project/urls.py
+++ bio_project/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("", include("spotify.urls")),
-    path("", include("filler.urls"))
+    path("", include("spotify.urls")),
+    #path("", include("filler.urls"))
 ]
